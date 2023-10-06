f37666a9b3597834561cee16e7a8bdc87241cb10
kier-ious <kier.diemer@gmail.com>
2023-10-06 14:16:00 -0700

    .

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,40 @@
 #!/usr/bin/python3
 """Base Model"""
-from datetime import datetime
 import uuid
-from models import storage
+from datetime import datetime
 
 
-class BaseModel():
+class BaseModel:
     """Base class for the console clone"""
     def __init__(self, *args, **kwargs):
+        self.id = None
         if kwargs:
-
-            date_format = "%Y-%m-%dT%H:%M:%S.%f"
-            k_dict = kwargs.copy()
-            del k_dict["__class__"]
-            for key in k_dict:
-                if (key == "created_at" or key == "updated_at"):
-                    k_dict[key] = datetime.strptime(k_dict[key], date_format)
-            self.__dict__ = k_dict
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                        )
+                    setattr(self, key, value)
         else:
-            """Assigning a unique ID as a string"""
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
-            storage.new(self)
-
-    def __str__(self):
-        """Reformating string representation"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                    self.id, self.__dict__)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Updating the update_at attr with current datetime"""
-        self.updated_at = datetime.today()
-        storage.save(self)
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Making a new_dict with extra field for __class__"""
-        new_dict = self.__dict__.copy()
-        new_dict["__class__"] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
+        """Making a dictionary repr of this instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
 
-        return new_dict
+    def __str__(self):
+        """Reformating string repr"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
