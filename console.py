a3c8c9374cf295a933380f702b18893634563883
marktipton <6675@holbertonstudents.com>
2023-10-10 16:00:30 -0700

    changed tests for base models. took out pep8 test and serialization tests and added tests for methods updated at created at to dict and id

--- console.py
+++ console.py
@@ -48,6 +48,11 @@ class HBNBCommand(cmd.Cmd):
         else:
             class_name = arg.strip()
             if class_name in self.classes:
+                """
+                globals returns dictionary of current symbol
+                table and instantiates object of type class_name
+                input by user
+                """
                 new_instance = globals()[class_name]()
                 new_instance.save()
                 print(new_instance.id)
